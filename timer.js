68e932abef3bf9bc203f5132ce7eddd59b20d194
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2023-10-10 00:51:24 -0300

    bugs fixed + funcionalidades

--- timer.js
+++ timer.js
@@ -1,199 +1,149 @@
-let timer;
-let startTime;
-let isRunning = false;
-let isSpacePressed = false;
+const startStopButton = document.getElementById('start-stop-button');
+const saveTimeButton = document.getElementById('save-time-button');
+const cubeTimer = document.getElementById('cube-timer');
+const tituloTabela = document.getElementById('titulo-tabela');
+const tabelaDeTempos = document.getElementById('tabela-de-tempos');
+
+let startTime = 0;
+let intervalId = null;
+let isTimerRunning = false; 
 let elapsedTime = 0;
 
-const cubeTimer = document.getElementById('cube-timer');
-const startStopButton = document.getElementById('start-stop-button');
+//pegar a barra de espaço
+document.body.addEventListener('keyup', function(e) {
+  if (e.keyCode === 32) {
+      toggleTimer(); 
+  }
+});
 
-startStopButton.addEventListener('click', () => {
-  if (!isRunning) {
-    if (isSpacePressed) {
-      elapsedTime = 0;
-    }
-    startTime = Date.now() - elapsedTime;
-    timer = setInterval(updateTimer, 10);
-    isRunning = true;
-    startStopButton.innerText = 'Pausar';
+startStopButton.addEventListener('click', toggleTimer);
+saveTimeButton.addEventListener('click', saveTime);
+
+//trocar a cor do botao ao rodar o timer
+function toggleTimer() {
+  if (!isTimerRunning) {
+      startTime = Date.now() - elapsedTime;
+      intervalId = setInterval(updateTimerDisplay, 10);
+      isTimerRunning = true;
+      startStopButton.innerHTML = '<i class="fas fa-pause"></i>';
+      startStopButton.classList.remove('btn-success'); 
+      startStopButton.classList.add('btn-outline-warning');
   } else {
-    clearInterval(timer);
-    isRunning = false;
-    startStopButton.innerText = 'Iniciar';
+      clearInterval(intervalId);
+      isTimerRunning = false;
+      startStopButton.innerHTML = '<i class="fas fa-play"></i>';
+      startStopButton.classList.remove('btn-outline-warning');   
+      startStopButton.classList.add('btn-success');    
   }
-});
+}
 
-window.addEventListener('keydown', (event) => {
-  if (event.key === ' ' && !isRunning) {
-    isSpacePressed = true;
-  }
-});
-
-window.addEventListener('keyup', (event) => {
-  if (event.key === ' ' && isSpacePressed) {
-    if (!isRunning) {
-      elapsedTime = 0;
-      startTime = Date.now();
-      displayTime(0);
-    } else {
-      clearInterval(timer);
-      isRunning = false;
-      startStopButton.innerText = 'Iniciar';
-    }
-    isSpacePressed = false;
-  }
-});
-
-function updateTimer() {
-  const currentTime = Date.now();
-  elapsedTime = currentTime - startTime;
-  displayTime(elapsedTime);
+function updateTimerDisplay() {
+    elapsedTime = Date.now() - startTime;
+    displayTime(elapsedTime);
 }
 
 function displayTime(time) {
-  const minutes = Math.floor(time / 60000);
-  const seconds = ((time % 60000) / 1000).toFixed(2);
-  cubeTimer.innerText = `${minutes}:${seconds}`;
+    const minutes = Math.floor(time / 60000);
+    const seconds = ((time % 60000) / 1000).toFixed(2);
+    cubeTimer.innerText = `${minutes}:${seconds}`;
 }
 
+//botao reset
 document.getElementById('reset-button').addEventListener('click', () => {
-  if (!isRunning) {
-    elapsedTime = 0;
-    displayTime(0);
-  }
-});
-
-// Tema escuro
-
-function myFunction() {
-  var element = document.body;
-  element.dataset.bsTheme =
-    element.dataset.bsTheme == 'light' ? 'dark' : 'light';
-}
-
-function stepFunction(event) {
-  debugger;
-  var element = document.getElementsByClassName('collapse');
-  for (var i = 0; i < element.length; i++) {
-    if (element[i] !== event.target.ariaControls) {
-      element[i].classList.remove('show');
+    if (!isTimerRunning) {
+        elapsedTime = 0;
+        displayTime(0);
     }
-  }
-}
-
-const saveTimeButton = document.getElementById('save-time-button');
-saveTimeButton.addEventListener('click', () => {
-  const tempoSalvo = elapsedTime;
-  let savedTimes = JSON.parse(localStorage.getItem('savedTimes')) || [];
-  savedTimes.push(tempoSalvo);
-
-  localStorage.setItem('savedTimes', JSON.stringify(savedTimes));
-  atualizarTabelaDeTempos(savedTimes);
 });
 
-function atualizarTabelaDeTempos(savedTimes) {
-  const tabela = document.getElementById('tabela-de-tempos');
-  const tbody = tabela.querySelector('tbody');
-
-  tbody.innerHTML = '';
-
-  savedTimes.forEach((tempo, indice) => {
-    const novaLinha = document.createElement('tr');
-    novaLinha.innerHTML = `
-      <td>${indice + 1}</td>
-      <td>${formatarTempo(tempo)}</td>
-    `;
-    tbody.appendChild(novaLinha);
-  });
-
- 
-  const tituloTabela = document.getElementById('titulo-tabela');
-  tituloTabela.innerText = 'Tempos Salvos';
-
-  tabela.style.display = 'table';
+//salvar tempo tabela
+function saveTime() {
+    let savedTimes = JSON.parse(localStorage.getItem('savedTimes')) || [];
+    savedTimes.push(elapsedTime);
+    localStorage.setItem('savedTimes', JSON.stringify(savedTimes));
+    updateSavedTimesTable(savedTimes);
+    tituloTabela.innerText = `Tempo Salvo: ${formatarTempo(elapsedTime)}`;
 }
 
+function updateSavedTimesTable(tempos) {
+    const tbody = tabelaDeTempos.querySelector('tbody');
+    tbody.innerHTML = '';
+    tempos.forEach((tempo, indice) => {
+        const novaLinha = document.createElement('tr');
+        novaLinha.innerHTML = `
+            <td>${indice + 1}</td>
+            <td>${formatarTempo(tempo)}</td>
+        `;
+        tbody.appendChild(novaLinha);
+    });
+}
 
 function formatarTempo(tempo) {
-  const minutos = Math.floor(tempo / 60000);
-  const segundos = ((tempo % 60000) / 1000).toFixed(2);
-  return `${minutos}:${segundos}`;
-}
-const clearTableButton = document.getElementById('clear-table-button');
-
-clearTableButton.addEventListener('click', () => {
-    const tbody = document.getElementById('tabela-de-tempos').querySelector('tbody');
-    while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-    }
-    localStorage.removeItem('savedTimes');
-});
-
-saveTimeButton.addEventListener('click', () => {
-  const tempoSalvo = elapsedTime;
-
-  const tituloTabela = document.getElementById('titulo-tabela');
-  tituloTabela.innerText = `Tempo Salvo: ${formatarTempo(tempoSalvo)}`;
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const tabelaDeTempos = document.getElementById('tabela-de-tempos');
-  const tbody = tabelaDeTempos.querySelector('tbody');
-
-  const temposSalvos = JSON.parse(localStorage.getItem('savedTimes')) || [];
-
-  temposSalvos.forEach((tempo, indice) => {
-      const newRow = document.createElement('tr');
-      newRow.innerHTML = `
-          <td>${indice + 1}</td>
-          <td>${formatarTempo(tempo)}</td>
-      `;
-      tbody.appendChild(newRow);
-  });
-
-  tabelaDeTempos.style.display = 'table';
-});
-
-const ordenarSelect = document.getElementById('ordenar-tabela');
-const tabelaDeTempos = document.getElementById('tabela-de-tempos');
-const temposSalvos = JSON.parse(localStorage.getItem('savedTimes')) || [];
-
-ordenarSelect.addEventListener('change', () => {
-  const ordem = ordenarSelect.value;
-  const temposOrdenados = [...temposSalvos];
-
-  if (ordem === 'asc') {
-    temposOrdenados.sort((a, b) => a - b); 
-  } else if (ordem === 'desc') {
-    temposOrdenados.sort((a, b) => b - a); 
-  }
-
-  atualizarTabelaDeTempos(temposOrdenados);
-});
-
-function atualizarTabelaDeTempos(tempos) {
-  const tbody = tabelaDeTempos.querySelector('tbody');
-  tbody.innerHTML = '';
-
-  tempos.forEach((tempo, indice) => {
-    const novaLinha = document.createElement('tr');
-    novaLinha.innerHTML = `
-      <td>${indice + 1}</td>
-      <td>${formatarTempo(tempo)}</td>
-    `;
-    tbody.appendChild(novaLinha);
-  });
+    const minutes = Math.floor(tempo / 60000);
+    const seconds = ((tempo % 60000) / 1000).toFixed(2);
+    return `${minutes}:${seconds}`;
 }
 
+//validacao apagar itens tabela 
 document.getElementById('clear-table-button').addEventListener('click', () => {
-
   const confirmClearModal = new bootstrap.Modal(document.getElementById('confirmClearModal'));
   confirmClearModal.show();
   document.getElementById('confirmClearButton').addEventListener('click', () => {
-      const tabela = document.getElementById('tabela-de-tempos');
-    
       localStorage.removeItem('savedTimes');
       confirmClearModal.hide();
+      updateSavedTimesTable([]);
   });
 });
 
+
+function mergeSort(array) {
+  if (array.length <= 1) {
+      return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+          result.push(left[leftIndex]);
+          leftIndex++;
+      } else {
+          result.push(right[rightIndex]);
+          rightIndex++;
+      }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const ordenarSelect = document.getElementById('ordenar-tabela');
+
+ordenarSelect.addEventListener('change', () => {
+  let savedTimes = JSON.parse(localStorage.getItem('savedTimes')) || [];
+  if (ordenarSelect.value === 'asc') {
+      savedTimes = mergeSort(savedTimes);
+  } else if (ordenarSelect.value === 'desc') {
+      savedTimes = mergeSort(savedTimes).reverse();
+  }
+  updateSavedTimesTable(savedTimes);
+});
+
+updateSavedTimesTable(JSON.parse(localStorage.getItem('savedTimes')) || []);
+
+
+//tema
+
+function toggleTheme() {
+  const element = document.body;
+  element.dataset.bsTheme = element.dataset.bsTheme === 'light' ? 'dark' : 'light';
+}
